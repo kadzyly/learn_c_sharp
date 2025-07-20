00aaa5c0cc4de92e3c9b7ca07f9ac1fa5561cd64
KadzyLy <kadzyly@gmail.com>
2025-07-20 18:41:15 +0200

    add comparison operators

---
+++ comparison-operators.cs
@@ -0,0 +1,26 @@
+
+int a = 10;
+int b = 4;
+
+
+// == equal
+bool c = a == b; // false
+
+// != not equal
+bool c = a != b;    // true
+bool d = a != 10;     // false
+
+// < less than
+bool c = a < b; // false
+
+// > more than
+bool c = a > b;     // true
+bool d = a > 25;    // false
+
+// <=
+bool c = a <= b;     // false
+bool d = a <= 25;    // true
+
+// >=
+bool c = a >= b;     // true
+bool d = a >= 25;    // false
