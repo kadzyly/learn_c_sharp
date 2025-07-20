44ce4a5c1971a8b22dbfac5051d2392222005254
KadzyLy <kadzyly@gmail.com>
2025-07-20 18:40:14 +0200

    add variables

---
+++ variables.cs
@@ -0,0 +1,47 @@
+
+
+// System.String. Unicode
+string sampleString = "Hello World";
+
+// System.Char. Unicode. 2 bytes
+char sampleChar = 'a';
+char sampleChar2 = '\u0420';
+
+// -----------------
+// System.Boolean. true / false
+bool sampleBool = true;
+
+
+// -----------------
+// System.Byte. from 0 to 255. 1 byte
+byte = sampleByte = 5; 
+
+// System.SByte. from -128 to 127. 1 byte
+sbyte sampleSByte = 5;
+
+// System.Int16. from -32768 to 32767. 2 bytes
+short sampleShort = 6;
+
+// System.UInt16. from 0 to 65535. 2 bytes
+ushort sampleUShort = 7;
+
+// System.Int32. from -2147483648 to 2147483647. 4 bytes
+int sampleInt = 5;
+
+// System.UInt32. from 0 to 4294967295. 4 bytes
+uint sampleUInt = 7;
+
+// System.Int64. from –9 223 372 036 854 775 808   to  9 223 372 036 854 775 807. 8 bytes
+long sampleLong = 8;
+
+// System.UInt64. from 0 to 18 446 744 073 709 551 615. 8 bytes
+ulong sampleULong = 9;
+
+// System.Single. from -3.4*10^38 to 3.4*10^38. 4 bytes
+float sampleFloat = 10.5f;
+
+// System.Double. ±5.0*10^-324 до ±1.7*10^308. 8 bytes
+double sampleDouble = 10.5;
+
+// -----------------
+int[] sampleArray = [ 1, 2, 3 ];
